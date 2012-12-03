1c5360297f31b3c53abf983f37158c956b057403
bybzmt <bybzmt@gmail.com>
2012-12-03 17:55:51 +0800

    edit

---
+++ webroot/admin/index.php
@@ -0,0 +1,21 @@
+<?php
+namespace Example;
+
+use By;
+
+require __DIR__ .'/../bootstrap.php';
+
+$input = new By\Input\Http(
+	INPUT_GET, array(
+		'type' => array('filter' => FILTER_SANITIZE_NUMBER_INT),
+		'page' => array('filter' => FILTER_SANITIZE_NUMBER_INT),
+	)
+);
+
+$controller = new By\Controller($input, new Output\Html('index'));
+
+$controller->addValid('valid', new Valid\Index());
+
+$controller->addAction('action', new Action\Index());
+
+$controller->run();
