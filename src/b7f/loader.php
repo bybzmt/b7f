f1c26f29bb4a4a97a43474a7158504ee842b8151
bybzmt <bybzmt@gmail.com>
2012-12-28 10:03:56 +0800

    loader

--- src/b7f/loader.php
+++ src/b7f/loader.php
@@ -1,30 +1,12 @@
 <?php
-namespace By;
+namespace b7f;
 
 class Loader
 {
 	/**
 	 * 命名空间
-	 * array(
-	 *     namespace1 => path1
-	 *     namespace2 => path2
-	 *     namespace3 => array(
-	 *        'path3',
-	 *        'names3-1' => array(
-	 *            'path-3-1',
-	 *            'names3-1-1 => 'path3-1-1'
-	 *            )
-	 *         )
-	 *     ...
-	 * )
 	 */
 	public $namespace = array();
-
-	/**
-	 * 替换规则
-	 */
-	public $search = array('\\');
-	public $replace = array(DIRECTORY_SEPARATOR);
 
 	public function __construct($namespace)
 	{
@@ -32,29 +14,9 @@ class Loader
 	}
 
 	/**
-	 * 定位一个类的文件
-	 *
-	 * @param string $classname 类名
-	 * @return 返回文件名
+	 * 载入一个类
 	 */
-	public function locate($classname)
-	{
-		$classname = strtolower($classname);
-
-		foreach ($this->namespace as $namespace => $path) {
-			if (strpos($classname, $namespace) === 0) {
-				return $path . str_replace(
-					'\\',
-					DIRECTORY_SEPARATOR,
-					substr($classname, strlen($namespace))
-				) . '.php';
-			}
-		}
-
-		return null;
-	}
-
-	public function locate3($classname)
+	public function load($classname)
 	{
 		$space = strtolower($classname);
 		$class = '';
@@ -63,44 +25,12 @@ class Loader
 
 		while ($tmp) {
 			if (isset($this->namespace[$space])) {
-				return $this->namespace[$space] . $class . '.php'; 
+				require $this->namespace[$space] . $class . '.php'; 
 			}
 
 			$class .= DIRECTORY_SEPARATOR . array_pop($tmp);
 			$space = implode('\\', $tmp);
 		}
-
-		return null;
-	}
-
-	/**
-	 * 载入一个类
-	 */
-	public function load($classname)
-	{
-		$file = $this->locate($classname);
-
-		if (file_exists($file)) {
-			require $file;
-		}
-	}
-
-	/**
-	 * 测试类文件是否存在
-	 */
-	public function fileExists($classname)
-	{
-		return file_exists($this->locate($classname));
-	}
-
-	/**
-	 * 尝试自动载入然后测试类是否存在
-	 */
-	public function exists($classname)
-	{
-		$this->load($classname);
-
-		return class_exists($classname, false);
 	}
 
 	/**
